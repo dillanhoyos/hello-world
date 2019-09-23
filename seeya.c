d16fa74b27a8260774ec7b20a43741bddd4d8b96
dillan <dillanhoyosmusic@gmail.com>
2019-09-23 19:14:11 -0400

    Create seeya.c

---
+++ seeya.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ask Me How Do I Feel?\a\n");
+    printf("Ask Me Now\a\n");
+    printf("That We're Cozy\a\n");
+    printf("And Clinging\a\n");
+    return 0;
+}
+
